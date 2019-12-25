e13d457562e177689b6f1e05605c8f11588e4ed3
Xx、 <1064248829@qq.com>
2019-12-25 14:25:46 +0800

    第一次提交项目源码

---
+++ house-biz/src/main/java/com/mooc/house/biz/mapper/BlogMapper.java
@@ -0,0 +1,20 @@
+package com.mooc.house.biz.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.mooc.house.common.model.Blog;
+import com.mooc.house.common.page.PageParams;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface BlogMapper {
+
+  List<Blog> selectBlog(@Param("blog")Blog query, @Param("pageParams")PageParams params);
+
+  Long selectBlogCount(Blog query);
+
+}
---
+++ house-biz/src/main/java/com/mooc/house/biz/mapper/HouseMapper.java
@@ -0,0 +1,44 @@
+package com.mooc.house.biz.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.mooc.house.common.model.Community;
+import com.mooc.house.common.model.House;
+import com.mooc.house.common.model.HouseUser;
+import com.mooc.house.common.model.User;
+import com.mooc.house.common.model.UserMsg;
+import com.mooc.house.common.page.PageParams;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface HouseMapper {
+
+	List<House>  selectPageHouses(@Param("house")House house,@Param("pageParams")PageParams pageParams);
+    
+	Long selectPageCount(@Param("house") House query);
+	
+	int insert(User account);
+
+	List<Community> selectCommunity(Community community);
+
+	int insert(House house);
+
+	HouseUser selectHouseUser(@Param("userId")Long userId,@Param("id") Long houseId,@Param("type") Integer integer);
+	
+	HouseUser selectSaleHouseUser(@Param("id") Long houseId);
+
+	int insertHouseUser(HouseUser houseUser);
+
+	int insertUserMsg(UserMsg userMsg);
+
+	int updateHouse(House updateHouse);
+	
+	int downHouse(Long id);
+
+	int deleteHouseUser(@Param("id")Long id,@Param("userId") Long userId,@Param("type") Integer value);
+	
+}
